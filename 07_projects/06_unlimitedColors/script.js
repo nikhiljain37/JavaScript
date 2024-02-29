7bacaca8d77ab2b08b66f38082b6739c786dba81
Nikhil Jain <97171526+nikhiljain37@users.noreply.github.com>
2024-02-29 08:35:09 +0000

    api project complete

---
+++ 07_projects/06_unlimitedColors/script.js
@@ -0,0 +1,31 @@
+// generate a random color
+
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = '#'
+    for(let i = 0 ; i < 6; i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let intervalId;
+
+const startChangingColor = function(){
+    // for edge caseswhen user click start and again start for that 
+   if(!intervalId) {
+    intervalId = setInterval(changeBgColor ,1000);
+   }
+    function changeBgColor(){
+        document.body.style.backgroundColor =randomColor();
+    }
+}
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null; // just for clear the code & the memory
+}
+
+document.getElementById('start').addEventListener('click' , startChangingColor);
+document.getElementById('stop').addEventListener('click' , stopChangingColor);
+
